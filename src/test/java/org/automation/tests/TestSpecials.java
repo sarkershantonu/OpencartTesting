487d8b069f1368e775c1d349190a98ae1de68e8c
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-13 01:35:39 -0500

    dummy test class for test specials

--- src/test/java/org/automation/tests/TestSpecials.java
+++ src/test/java/org/automation/tests/TestSpecials.java
@@ -1,7 +1,9 @@
 package org.automation.tests;
 
+import org.automation.core.TestBase;
+
 /**
  * Created by shantonu on 12/13/16.
  */
-public class TestSpecials {
+public class TestSpecials extends TestBase {
 }
