2c9a19fe39163ed2aa78a30fd1857f3f1108a13f
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-13 01:44:08 -0500

    Suit execution example via maven properties inside  Surefire plugins.

--- src/test/java/org/automation/suits/RegressionTests.java
+++ src/test/java/org/automation/suits/RegressionTests.java
@@ -1,5 +1,6 @@
 package org.automation.suits;
 
+import org.automation.CucumberTest;
 import org.automation.tests.TestMenuItems;
 import org.automation.tests.TestSearch;
 import org.junit.runner.RunWith;
@@ -9,6 +10,6 @@ import org.junit.runners.Suite;
  * Created by shantonu on 12/10/16.
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TestMenuItems.class, TestSearch.class})
+@Suite.SuiteClasses({CucumberTest.class})
 public class RegressionTests {
 }
