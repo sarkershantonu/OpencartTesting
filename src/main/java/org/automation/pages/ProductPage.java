83d913e040630a47434e3da28858ef239fdca3c8
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-14 08:28:33 -0500

    product preview

---
+++ src/main/java/org/automation/pages/ProductPage.java
@@ -0,0 +1,13 @@
+package org.automation.pages;
+
+import org.automation.core.PageBase;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by shantonu on 12/14/16.
+ */
+public class ProductPage extends PageBase {
+    public ProductPage(WebDriver aDriver) {
+        super(aDriver);
+    }
+}
---
+++ src/main/java/org/automation/pages/ProductPreviewPage.java
@@ -0,0 +1,18 @@
+package org.automation.pages;
+
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by shantonu on 12/14/16.
+ */
+// this will be on the fly item creation.. so all item will be dynamic.. need to make a dom parser.
+public class ProductPreviewPage {
+    public WebElement tile;
+    public WebElement description;
+    public WebElement price;
+    public WebElement tax;
+    public WebElement addToCartLinik;
+    public WebElement faviorateLinik;
+    public WebElement compareProductLinik;
+
+}
