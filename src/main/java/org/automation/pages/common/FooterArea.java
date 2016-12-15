ee78f58a7e53a9d86c17e1eec0ef94d2ac8ca1b4
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-14 23:57:08 -0500

    about us

--- src/main/java/org/automation/pages/common/FooterArea.java
+++ src/main/java/org/automation/pages/common/FooterArea.java
@@ -2,6 +2,7 @@ package org.automation.pages.common;
 
 import org.automation.core.PageBase;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 /**
  * Created by shantonu on 12/14/16.
@@ -11,4 +12,7 @@ public class FooterArea extends PageBase {
     public FooterArea(WebDriver aDriver) {
         super(aDriver);
     }
+
+    public WebElement aboutUsLink;
+
 }
