2cac75858b668cc2c227923c86ab02f681d6055e
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-16 20:45:43 -0500

    header items

--- src/main/java/org/automation/pages/common/HeaderArea.java
+++ src/main/java/org/automation/pages/common/HeaderArea.java
@@ -2,11 +2,28 @@ package org.automation.pages.common;
 
 import org.automation.core.PageBase;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 /**
  * Created by shantonu on 12/14/16.
  */
 public class HeaderArea extends PageBase {
+
+    public WebElement checkoutLink;
+    public WebElement shoppingCartLink;
+    public WebElement Link;
+    public WebElement siteMapLink;
+    public WebElement brandsLink;
+    public WebElement giftVoucersLink;
+    public WebElement affiliatesLink;
+    public WebElement specialsLink;
+    public WebElement myAccountLink;
+    public WebElement OrderHistoryLink;
+
+    public WebElement wishListLink;
+    public WebElement newsLetterLink;
+
+
     public HeaderArea(WebDriver aDriver) {
         super(aDriver);
     }
