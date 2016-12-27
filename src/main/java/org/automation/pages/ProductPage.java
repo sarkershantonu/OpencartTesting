d072032963f1025cc78d84cc528102ff1eb36050
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-27 00:00:19 -0500

    sample product page to see iphone

--- src/main/java/org/automation/pages/ProductPage.java
+++ src/main/java/org/automation/pages/ProductPage.java
@@ -2,6 +2,9 @@ package org.automation.pages;
 
 import org.automation.core.PageBase;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
 /**
  * Created by shantonu on 12/14/16.
@@ -10,4 +13,7 @@ public class ProductPage extends PageBase {
     public ProductPage(WebDriver aDriver) {
         super(aDriver);
     }
+
+    @FindBy(how=How.LINK_TEXT, linkText = "Description")
+    public WebElement description_link;
 }
