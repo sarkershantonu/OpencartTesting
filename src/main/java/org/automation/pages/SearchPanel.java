ac24778dc5580b64d2269e1ec02a84b959bdcb44
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-27 00:33:48 -0500

    locators for search page

--- src/main/java/org/automation/pages/SearchPanel.java
+++ src/main/java/org/automation/pages/SearchPanel.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.FindBy;
 
 /**
  * Created by shantonu on 5/17/16.
+ * todo item added for all locators
  */
 public class SearchPanel extends PageBase {
 
@@ -20,4 +21,61 @@ public class SearchPanel extends PageBase {
     public SearchPanel(WebDriver aDriver) {
         super(aDriver);
     }
+
+
+    @FindBy(xpath = "")
+    public WebElement searchResult_text;
+
+    @FindBy(xpath = "")
+    public WebElement searchCriteria_text;
+
+    @FindBy(xpath = "")
+    public WebElement serchKeywordTextBox;
+
+    @FindBy(xpath = "")
+    public WebElement calatagoryCombo;
+
+    @FindBy(xpath = "")
+    public WebElement searchInSub_checkBox;
+
+    @FindBy(xpath = "")
+    public WebElement searchInDescription_checkBox;
+
+    @FindBy(xpath = "")
+    public WebElement searchButton;
+
+    @FindBy(xpath = "")
+    public WebElement searchResult_title_text;
+
+    @FindBy(xpath = "")
+    public WebElement sortBy_text;
+
+    @FindBy(xpath = "")
+    public WebElement sortByCombo;
+
+    @FindBy(xpath = "")
+    public WebElement show_text;
+
+    @FindBy(xpath = "")
+    public WebElement showCombo;
+
+    @FindBy(xpath = "")
+    public WebElement productCompare_text;
+
+    @FindBy(xpath = "")
+    public WebElement productThumbrnelbutton_grid;
+
+    @FindBy(xpath = "")
+    public WebElement getProductThumbrnelbutton_list;
+
+    @FindBy(xpath = "")
+    public WebElement resultThumbnail_2;
+
+    @FindBy(xpath = "")
+    public WebElement resultThumbnail_3;
+    @FindBy(xpath = "")
+    public WebElement resultThumbnail_4;
+    @FindBy(xpath = "")
+    public WebElement resultThumbnail_1;
+
 }
