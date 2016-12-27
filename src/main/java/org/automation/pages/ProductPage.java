e09bcd41420e7a6cc5d12b29368d7a09de7e02e7
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-27 00:18:12 -0500

    todo added for a product page

--- src/main/java/org/automation/pages/ProductPage.java
+++ src/main/java/org/automation/pages/ProductPage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.How;
 
 /**
  * Created by shantonu on 12/14/16.
+ * todo : http://demo.opencart.com/index.php?route=product/product&product_id=40
  */
 public class ProductPage extends PageBase {
     public ProductPage(WebDriver aDriver) {
@@ -23,12 +24,30 @@ public class ProductPage extends PageBase {
 
     public WebElement wishList_link;
     public WebElement compare_link;
+    public WebElement brand_link;
+    public WebElement avaiability_text;
+    public WebElement productCode_text;
+    public WebElement productTitle_text;
+    public WebElement productPrice;
+    public WebElement productTax;
     public WebElement addToCart_button;
     public WebElement quantity_textbox;
+    public WebElement continue_review_button;
 
     public boolean writeAReview(String name, String review, int rating){
 
         return false;
+    }
+
+    /**
+     * This will get all links from a loaded page.
+     * @return
+     * todo
+     */
+    public String[] getAllImageLinks(){
+        final String source = driver.getPageSource();
+        String pattern = "class=\"thumbnail\"\\shref=\"(.+?jpg)";
+        return null;
     }
 
 }
