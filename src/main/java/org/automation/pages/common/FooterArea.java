50455aa92af32b3090793c0b084ae171212439af
JyotiProkash <cse0718@gmail.com>
2017-01-25 00:08:57 +0600

    Added all locators for FooterArea class

--- src/main/java/org/automation/pages/common/FooterArea.java
+++ src/main/java/org/automation/pages/common/FooterArea.java
@@ -3,36 +3,53 @@ package org.automation.pages.common;
 import org.automation.core.PageBase;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
 /**
  * Created by shantonu on 12/14/16.
  * todo, all element locators
+ */
+
+/**
+ * Added all element locators
  */
 public class FooterArea extends PageBase {
 
     public FooterArea(WebDriver aDriver) {
         super(aDriver);
     }
-
+    @FindBy(how= How.LINK_TEXT, linkText = "About Us")
     public WebElement aboutUsLink;
-
+    @FindBy(how= How.LINK_TEXT, linkText = "Delivery Information")
     public WebElement deliveryInfoLink;
-
+    @FindBy(how= How.LINK_TEXT, linkText = "Privacy Policy")
     public WebElement privacyPolicyLink;
-
+    @FindBy(how= How.LINK_TEXT, linkText = "Terms & Conditions")
     public WebElement termsAndConditionLink;
+    @FindBy(how= How.LINK_TEXT, linkText = "Contact Us")
     public WebElement contactUsLink;
+    @FindBy(how= How.LINK_TEXT, linkText = "Returns")
     public WebElement returnssLink;
+    @FindBy(how= How.LINK_TEXT, linkText = "Site Map")
     public WebElement siteMapLink;
+    @FindBy(how= How.LINK_TEXT, linkText = "Brands")
     public WebElement brandsLink;
+    @FindBy(how= How.LINK_TEXT, linkText = "Gift Vouchers")
     public WebElement giftVoucersLink;
+    @FindBy(how= How.LINK_TEXT, linkText = "Affiliates")
     public WebElement affiliatesLink;
+    @FindBy(how= How.LINK_TEXT, linkText = "Specials")
     public WebElement specialsLink;
+    @FindBy(how= How.LINK_TEXT, linkText = "My Account")
     public WebElement myAccountLink;
+    @FindBy(how= How.LINK_TEXT, linkText = "Order History")
     public WebElement OrderHistoryLink;
+    @FindBy(how= How.LINK_TEXT, linkText = "Wish List")
     public WebElement wishListLink;
+    @FindBy(how= How.LINK_TEXT, linkText = "Newsletter")
     public WebElement newsLetterLink;
-
+    @FindBy(how= How.XPATH, xpath = ".//p[contains(text(), 'Powered By')]")
     public WebElement copyright;
 
 }
