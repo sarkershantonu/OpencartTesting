41bdc8b287f57ffbf4de24bd30a802afe952c6a7
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-13 00:02:09 -0400

    property loader

---
+++ src/main/java/org/automation/App.java
@@ -0,0 +1,19 @@
+package org.automation;
+
+import org.automation.config.PropertyLoader;
+
+import java.io.IOException;
+
+/**
+ * Created by shantonu on 3/13/17.
+ */
+public class App {
+    public void initProperties(){
+        try {
+            PropertyLoader.loadProperties("./src/test/driver.properties");
+            PropertyLoader.loadProperties("./src/test/test.properties");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
