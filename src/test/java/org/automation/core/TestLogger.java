25adb753d7285680705d4f98c910ea0cd5202494
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-15 23:57:42 -0400

    junit watcher

--- src/test/java/org/automation/core/TestLogger.java
+++ src/test/java/org/automation/core/TestLogger.java
@@ -1,7 +1,10 @@
 package org.automation.core;
 
+import org.junit.rules.TestWatcher;
+
 /**
  * Created by shantonu on 3/15/17.
  */
-public class TestLogger {
+public class TestLogger extends TestWatcher {
+
 }
