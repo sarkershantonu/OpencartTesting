bff06848a805cd474e9c1f3dfd1e469c4f388385
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-15 23:58:55 -0400

    watcher overrid

--- src/test/java/org/automation/core/TestLogger.java
+++ src/test/java/org/automation/core/TestLogger.java
@@ -6,5 +6,46 @@ import org.junit.rules.TestWatcher;
  * Created by shantonu on 3/15/17.
  */
 public class TestLogger extends TestWatcher {
+    private static Logger logger;
+
+    public LoggingRule(Logger logger) {
+        this.logger = logger;
+    }
+
+    @Override
+    protected void starting(Description description) {
+        super.starting(description);
+        logger.info("TEST Started : "+ description.getMethodName());
+    }
+
+    /**
+     * Invoked when a test method finishes (whether passing or failing)
+     */
+    @Override
+    protected void finished(Description description) {
+        super.finished(description);
+        logger.info("TEST COMPLETE : "+ description.getMethodName());
+    }
+
+    /**
+     * invoke for test pass
+     * @param description
+     */
+    @Override
+    protected void succeeded(Description description) {
+        super.succeeded(description);
+
+        logger.info("TEST SUCCESS : "+ description.getMethodName());
+    }
+
+    /**
+     * Invoked when a test fails
+     */
+
+    @Override
+    protected void failed(Throwable e, Description description) {
+        super.failed(e,description);
+        logger.error("TEST FAILED >>> "+ description.getMethodName() +" and the Test Class ="+description.getClassName()+" , \n ERROR = "+ e.getMessage());
+    }
 
 }
