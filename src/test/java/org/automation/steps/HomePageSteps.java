efc49e25d381b1ead4c459d8596739fcc28db4a8
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-17 23:59:30 -0400

    home page steps basics

--- src/test/java/org/automation/steps/HomePageSteps.java
+++ src/test/java/org/automation/steps/HomePageSteps.java
@@ -2,7 +2,12 @@ package org.automation.steps;
 
 /**
  * Created by shantonu on 5/5/16.
+ * this is whole TODo list for home page
  */
 public class HomePageSteps {
 
+    public String getCopyRightInfoFromBottom(){
+        //The OpenCart demo store © 2017
+        return null;
+    }
 }
