b80f043ff4a7ad036dc2fb843bcb77bc7cbfe954
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-17 23:44:01 -0400

    looging happening in console

--- src/test/java/org/automation/steps/SearchStepDefination.java
+++ src/test/java/org/automation/steps/SearchStepDefination.java
@@ -24,19 +24,24 @@ public class SearchStepDefination extends StepBase {
     private ResultPage resultPage;
     private long start, end;
 
+
+    @Test
+    public void addingTest(){
+        Assert.assertEquals(true,true);
+    }
     @Rule
     public TestWatcher testLogger = new TestLogger(logger);
 
     @After
     public void finish(){
     }
-    @Test
+
     @Given("^I open (.+) browser$")
     public void i_open_browser(String nameOfBrowser) throws Throwable {
        driver = Browser.getInstance(nameOfBrowser);
     }
 
-    @Test
+
     @When("^I type (.+) and press enter$")
     public void i_type_http_demo_opencart_com_and_press_enter(String url) throws Throwable {
         home = new HomePage(driver);
@@ -45,12 +50,12 @@ public class SearchStepDefination extends StepBase {
         home.open(url);
     }
 
-    @Test
+
     @Then("^I can see page loaded with title \"([^\"]*)\"$")
     public void i_can_see_page_loaded_with_title(String arg1) throws Throwable {
         Assert.assertEquals(arg1,home.getTitle());
     }
-    @Test
+
     @Then("^I can see invalid search title with \"([^\"]*)\" and message \"([^\"]*)\"$")
     public void i_can_see_invalid_search_title_with_and_message(String arg1, String arg2) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
@@ -58,13 +63,13 @@ public class SearchStepDefination extends StepBase {
         Assert.assertEquals(arg2, resultPage.invalid_search_result_message.getText());
         end= System.currentTimeMillis();
     }
-    @Test
+
     @Then("^I can see search results with title \"([^\"]*)\"$")
     public void i_can_see_search_results_with_title(String arg1) throws Throwable {
         Assert.assertEquals(arg1,resultPage.getTitle());
         end= System.currentTimeMillis();
     }
-    @Test
+
     @And("^I type \"([^\"]*)\" in search box And I click search button$")
     public void i_type_in_Iphone_in_search_box_And_I_click_search(String word) throws Throwable {
 
@@ -75,12 +80,12 @@ public class SearchStepDefination extends StepBase {
     }
 
 
-    @Test
+
     @And("^I can see the search should not take more than (\\d+) second$")
     public void i_can_see_the_search_should_not_take_more_than_second(int arg1) throws Throwable {
         Assert.assertTrue((end-start)<(arg1*1000));
     }
-    @Test
+
     @Then("^I quit browser$")
     public void i_quit_browser() throws Throwable {
         Browser.close();
