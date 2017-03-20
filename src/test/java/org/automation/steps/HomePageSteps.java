93b921f2b15ebaac91e7440d2163e94c9b854f07
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-20 04:48:46 -0400

    step for page click

--- src/test/java/org/automation/steps/HomePageSteps.java
+++ src/test/java/org/automation/steps/HomePageSteps.java
@@ -1,32 +1,38 @@
 package org.automation.steps;
 
+import org.automation.core.Browser;
+import org.automation.core.PageBase;
+import org.automation.pages.HomePage;
+
 /**
  * Created by shantonu on 5/5/16.
  * this is whole TODo list for home page
  */
 public class HomePageSteps {
 
-    public void click_About_Us(){
+    private HomePage homePage;
+
+    public PageBase click_About_Us(){
 
     }
-    public void click_Delivery_Information(){
+    public PageBase click_Delivery_Information(){
 
     }
-    public void click_Privacy_Policy(){}
+    public PageBase click_Privacy_Policy(){}
 
-    public void click_Terms_Conditions(){}
+    public PageBase click_Terms_Conditions(){}
 
-    public void click_Returns (){}
+    public PageBase click_Returns (){}
 
-    public void click_SiteMap(){}
-    public void click_Brands(){}
-    public void click_Gift_Vouchers(){}
-    public void click_Affiliates (){}
-    public void click_Specials (){}
-    public void click_My_Account(){}
-    public void click_Order_History (){}
-    public void click_Wish_List(){}
-    public void click_Newsletter (){}
+    public PageBase click_SiteMap(){}
+    public PageBase click_Brands(){}
+    public PageBase click_Gift_Vouchers(){}
+    public PageBase click_Affiliates (){}
+    public PageBase click_Specials (){}
+    public PageBase click_My_Account(){}
+    public PageBase click_Order_History (){}
+    public PageBase click_Wish_List(){}
+    public PageBase click_Newsletter (){}
 
 
 }
