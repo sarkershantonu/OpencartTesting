a7a7e7fbe266e0599c59475abdc4840d026d3b31
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-30 01:51:40 -0400

    todo => delivery info

--- src/main/java/org/automation/pages/DeliveryInformation.java
+++ src/main/java/org/automation/pages/DeliveryInformation.java
@@ -2,6 +2,7 @@ package org.automation.pages;
 
 import org.automation.core.PageBase;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 /**
  * Created by shantonu on 3/20/17.
@@ -10,4 +11,7 @@ public class DeliveryInformation extends PageBase {
     public DeliveryInformation(WebDriver aDriver) {
         super(aDriver);
     }
+
+    public WebElement description;
+    public WebElement title;
 }
