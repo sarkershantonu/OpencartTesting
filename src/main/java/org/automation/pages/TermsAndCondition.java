faae5e5bca81166eb283aa2a838d2ce4617f1f9d
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-30 01:52:40 -0400

    todo => termd & condition

--- src/main/java/org/automation/pages/TermsAndCondition.java
+++ src/main/java/org/automation/pages/TermsAndCondition.java
@@ -2,6 +2,7 @@ package org.automation.pages;
 
 import org.automation.core.PageBase;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 /**
  * Created by shantonu on 3/20/17.
@@ -10,4 +11,8 @@ public class TermsAndCondition extends PageBase {
     public TermsAndCondition(WebDriver aDriver) {
         super(aDriver);
     }
+
+    //todo all locators
+    public WebElement description;
+    public WebElement title;
 }
