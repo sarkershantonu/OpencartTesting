dbec2f577bf09d9246e448a896ad1bae1e3cc1f9
sarkershantonu <shantonu_oxford@yahoo.com>
2017-04-04 00:23:57 -0400

    login

--- src/main/java/org/automation/pages/LoginPage.java
+++ src/main/java/org/automation/pages/LoginPage.java
@@ -2,6 +2,7 @@ package org.automation.pages;
 
 import org.automation.core.PageBase;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 /**
  * Created by shantonu on 4/3/17.
@@ -10,5 +11,17 @@ public class LoginPage extends PageBase {
     public LoginPage(WebDriver aDriver) {
         super(aDriver);
     }
+    public PageBase submit() {
+        clickLoginButton();
 
+        PageFactory.initElements(driver, this);
+        return this;
+    }
+
+    private void clickLoginButton() {
+    }
+    public LoginPage fill() {
+
+        return this;
+    }
 }
