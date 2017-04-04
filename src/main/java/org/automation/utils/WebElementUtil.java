99caeabd8063bc9769b642b842bcb4100ade666c
sarkershantonu <shantonu_oxford@yahoo.com>
2017-04-03 23:51:15 -0400

    new customar area login page

--- src/main/java/org/automation/utils/WebElementUtil.java
+++ src/main/java/org/automation/utils/WebElementUtil.java
@@ -1,6 +1,8 @@
 package org.automation.utils;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
 
 /**
  * Created by shantonu on 3/30/17.
@@ -9,4 +11,7 @@ public class WebElementUtil {
     public static String getValue(WebElement element, String attribute){
         return element.getAttribute(attribute);
     }
+    @FindBy(css = "a.btn.btn-primary")
+    @CacheLookup
+    private WebElement continue_btn;
 }
