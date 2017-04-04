dcc528d54a8ad76f611b5a0646aae8f09f0b6255
sarkershantonu <shantonu_oxford@yahoo.com>
2017-04-03 23:49:33 -0400

    login page

---
+++ src/main/java/org/automation/pages/LoginPage.java
@@ -0,0 +1,14 @@
+package org.automation.pages;
+
+import org.automation.core.PageBase;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by shantonu on 4/3/17.
+ */
+public class LoginPage extends PageBase {
+    public LoginPage(WebDriver aDriver) {
+        super(aDriver);
+    }
+
+}
