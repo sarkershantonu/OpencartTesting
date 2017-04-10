34d1e7908b5ce90324b5668f8d313dd47817e454
sarkershantonu <shantonu_oxford@yahoo.com>
2017-04-09 23:59:49 -0400

    menu refactored

--- src/main/java/org/automation/pages/common/Menu.java
+++ src/main/java/org/automation/pages/common/Menu.java
@@ -1,12 +1,20 @@
 package org.automation.pages.common;
 
-import org.automation.pages.OpenCartPageBase;
+import org.automation.core.PageBase;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
 
 /**
- * Created by shantonu on 12/27/16.
+ * Created by shantonu on 12/28/16.
  */
-public class Menu extends OpenCartPageBase {
+public class Menu extends PageBase {
+
+    @FindBy(css = "a[href='https://demo.opencart.com/index.php?route=product/category&path=17']")
+    @CacheLookup
+    private WebElement software;
+
     public Menu(WebDriver aDriver) {
         super(aDriver);
     }
