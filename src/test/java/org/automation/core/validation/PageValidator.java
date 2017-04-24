7e277919944ee7be9163cceda344b4ac0a0e6626
sarkershantonu <shantonu_oxford@yahoo.com>
2017-04-24 00:00:54 -0400

    validtor page base

--- src/test/java/org/automation/core/validation/PageValidator.java
+++ src/test/java/org/automation/core/validation/PageValidator.java
@@ -1,11 +1,18 @@
 package org.automation.core.validation;
 
+import org.automation.core.PageBase;
+
 /**
  * Created by shantonu on 4/23/17.
  */
 public class PageValidator {
 
-    public static boolean validatePageIsLoaded(){
+    public static boolean validatePageIsLoaded(<? extends PageBase>){
+        try{
 
+
+        }catch (AssertionError e){
+            return false;
+        }
     }
 }
