e2065fee4ea770329731b75b5989a622fed0bf3c
sarkershantonu <shantonu_oxford@yahoo.com>
2017-04-24 02:43:34 -0400

    page validator

--- src/test/java/org/automation/core/validation/PageValidator.java
+++ src/test/java/org/automation/core/validation/PageValidator.java
@@ -1,16 +1,18 @@
 package org.automation.core.validation;
 
+
 import org.automation.core.PageBase;
+import org.junit.Assert;
 
 /**
  * Created by shantonu on 4/23/17.
  */
 public class PageValidator {
 
-    public <T extends PageBase>boolean validatePageIsLoaded(T aPage){
+    public <T extends PageBase>boolean validatePageIsLoaded(T aPage, String expectedTitle){
         try{
-
-
+            Assert.assertEquals("Title are not same", expectedTitle, aPage.getTitle());
+            return true;
         }catch (AssertionError e){
             return false;
         }
