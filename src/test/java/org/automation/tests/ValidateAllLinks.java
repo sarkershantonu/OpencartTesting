34c382f3ab1b60987aa20c22fd14daee7fdb13d5
sarkershantonu <shantonu_oxford@yahoo.com>
2017-04-23 23:56:16 -0400

    steps for link validation

--- src/test/java/org/automation/tests/ValidateAllLinks.java
+++ src/test/java/org/automation/tests/ValidateAllLinks.java
@@ -1,9 +1,9 @@
 package org.automation.tests;
 
-import org.automation.core.TestBase;
+import org.automation.core.StepBase;
 
 /**
  * Created by shantonu on 4/23/17.
  */
-public class ValidateAllLinks extends TestBase {
+public class ValidateAllLinks extends StepBase {
 }
