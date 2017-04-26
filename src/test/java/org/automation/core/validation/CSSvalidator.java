3411b4696050d02ca8fc953e677f7fb324fa8952
sarkershantonu <shantonu_oxford@yahoo.com>
2017-04-26 02:35:36 -0400

    css validator constract

--- src/test/java/org/automation/core/validation/CSSvalidator.java
+++ src/test/java/org/automation/core/validation/CSSvalidator.java
@@ -1,11 +1,19 @@
 package org.automation.core.validation;
 
+import org.openqa.selenium.WebElement;
+
 /**
  * Created by shantonu on 12/10/16.
  */
 //this will validate all CSS from driver source.
     //TODO , see all CSS tags & create methods => http://www.w3schools.com/css/default.asp
 public class CSSvalidator {
+
+    private WebElement aWebElement;
+    public CSSvalidator(WebElement element){
+        aWebElement = element;
+    }
+    private CSSvalidator(){}
     public boolean isTagPresent(String tag){return false;}
     public boolean isClassPresent(String aClass){return false;}
     public boolean isIDPresent(String id){return false;}
