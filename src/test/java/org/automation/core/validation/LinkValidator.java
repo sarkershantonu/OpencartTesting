c0849e91aab1c2993b80138d41fd63bb3a258a92
sarkershantonu <shantonu_oxford@yahoo.com>
2017-04-25 23:52:25 -0400

    http client

--- src/test/java/org/automation/core/validation/LinkValidator.java
+++ src/test/java/org/automation/core/validation/LinkValidator.java
@@ -1,5 +1,13 @@
 package org.automation.core.validation;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 /**
@@ -18,7 +26,35 @@ public class LinkValidator {
      * @param url
      * @return
      */
-    public boolean isActiveLink(String url){
+    public boolean isActiveLink(String url) throws IOException {
+        //make a http request
+
+        String url = "http://www.google.com/search?q=httpClient";
+
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(url);
+
+// add request header
+        request.addHeader("User-Agent", USER_AGENT);
+        HttpResponse response = null;
+        try {
+            response = client.execute(request);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Response Code : "
+                + response.getStatusLine().getStatusCode());
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+
         return false;
     }
 }
