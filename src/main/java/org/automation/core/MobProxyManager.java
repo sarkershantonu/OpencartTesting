fb0555af6de99176a0dd18fa6df5fdb47ffbaf82
sarkershantonu <shantonu_oxford@yahoo.com>
2017-04-27 01:22:46 -0400

    browser mob example

--- src/main/java/org/automation/core/MobProxyManager.java
+++ src/main/java/org/automation/core/MobProxyManager.java
@@ -1,7 +1,23 @@
 package org.automation.core;
 
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+
 /**
  * Created by shantonu on 4/27/17.
  */
 public class MobProxyManager {
+
+    private MobProxyManager(){}
+
+    public static void startProxy(int port){
+        BrowserMobProxy proxy_local = new BrowserMobProxyServer();
+        proxy_local.start(port);
+    }
+
+    public static void main(String[] args) {
+        startProxy(9600);
+
+    }
+
 }
