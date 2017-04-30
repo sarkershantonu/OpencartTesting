eb9659dcd25b8535679d3923ecb52fa8fecf1384
sarkershantonu <shantonu_oxford@yahoo.com>
2017-04-29 23:50:06 -0400

    item common

--- src/main/java/org/automation/pages/common/ProductPageCommon.java
+++ src/main/java/org/automation/pages/common/ProductPageCommon.java
@@ -1,11 +1,17 @@
 package org.automation.pages.common;
 
+import org.automation.core.PageBase;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 /**
  * Created by shantonu on 4/27/17.
  */
-public class ProductPageCommon {
+public class ProductPageCommon extends PageBase{
 
     public WebElement description_tab;
+
+    public ProductPageCommon(WebDriver aDriver) {
+        super(aDriver);
+    }
 }
