f185c819a5eed1c3425864114742326232f3df81
sarkershantonu <shantonu_oxford@yahoo.com>
2017-04-29 23:49:34 -0400

    description

--- src/main/java/org/automation/pages/common/ProductPageCommon.java
+++ src/main/java/org/automation/pages/common/ProductPageCommon.java
@@ -1,7 +1,11 @@
 package org.automation.pages.common;
 
+import org.openqa.selenium.WebElement;
+
 /**
  * Created by shantonu on 4/27/17.
  */
 public class ProductPageCommon {
+
+    public WebElement description_tab;
 }
