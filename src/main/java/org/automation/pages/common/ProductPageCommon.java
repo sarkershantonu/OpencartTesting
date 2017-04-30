26cbcb0cd33f0ce079f3f9784a30a7329c5559a9
sarkershantonu <shantonu_oxford@yahoo.com>
2017-04-29 23:55:50 -0400

    add to cart

--- src/main/java/org/automation/pages/common/ProductPageCommon.java
+++ src/main/java/org/automation/pages/common/ProductPageCommon.java
@@ -3,6 +3,8 @@ package org.automation.pages.common;
 import org.automation.core.PageBase;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
 
 /**
  * Created by shantonu on 4/27/17.
@@ -13,6 +15,9 @@ public class ProductPageCommon extends PageBase{
     public WebElement review_tab;
     public WebElement addToWishList;
 
+    @FindBy(id = "button-cart")
+    @CacheLookup
+    private WebElement addToCart;
     public ProductPageCommon(WebDriver aDriver) {
         super(aDriver);
     }
