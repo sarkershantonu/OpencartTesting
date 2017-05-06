d67667755dd6db1985beeeb00159b5a8c0e6a5ec
sarkershantonu <shantonu_oxford@yahoo.com>
2017-05-05 23:54:56 -0400

    about us

--- src/main/java/org/automation/pages/AboutUs.java
+++ src/main/java/org/automation/pages/AboutUs.java
@@ -8,12 +8,15 @@ import org.openqa.selenium.support.FindBy;
 /**
  * Created by shantonu on 3/20/17.
  * //todo element finders
+ * /
  */
 public class AboutUs extends PageBase {
     public AboutUs(WebDriver aDriver) {
         super(aDriver);
     }
 //this represent About us page
+
+    @FindBy(css ="#content > p" )
     public WebElement description;
 
     @FindBy(css ="h1" )
