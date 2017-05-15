9743bd665291bb5026efbf961b032ae690537f01
sarkershantonu <shantonu_oxford@yahoo.com>
2017-05-14 23:54:29 -0400

    step design for menu

--- src/test/java/org/automation/core/StepBase.java
+++ src/test/java/org/automation/core/StepBase.java
@@ -1,11 +1,18 @@
 package org.automation.core;
 
 import org.junit.Rule;
+import org.openqa.selenium.WebDriver;
 
 /**
  * Created by shantonu on 3/16/17.
  * Base file for cucumber steps
  */
 public abstract class StepBase {
+
+    protected WebDriver driver;
+
+    public StepBase(){
+
+    }
 
 }
--- src/test/java/org/automation/steps/common/MenuItemSteps.java
+++ src/test/java/org/automation/steps/common/MenuItemSteps.java
@@ -1,9 +1,18 @@
 package org.automation.steps.common;
 
 import org.automation.core.StepBase;
+import org.automation.pages.common.Menu;
 
 /**
  * Created by shantonu on 4/24/17.
  */
 public class MenuItemSteps extends StepBase {
+    private Menu menu;
+
+    private MenuItemSteps(){}
+    public MenuItemSteps(Menu allMenu){
+        menu = allMenu;
+    }
+
+
 }
