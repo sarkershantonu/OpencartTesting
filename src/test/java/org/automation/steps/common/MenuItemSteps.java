ebafdd9226c260d821833f8d94736d61308a2132
sarkershantonu <shantonu_oxford@yahoo.com>
2017-05-16 23:45:57 -0400

    example go to link step

--- src/test/java/org/automation/steps/common/MenuItemSteps.java
+++ src/test/java/org/automation/steps/common/MenuItemSteps.java
@@ -1,5 +1,6 @@
 package org.automation.steps.common;
 
+import cucumber.api.java.en.Then;
 import org.automation.core.StepBase;
 import org.automation.pages.common.Menu;
 
@@ -16,7 +17,9 @@ public class MenuItemSteps extends StepBase {
         menu = allMenu;
     }
 
-    public void gotoLink() {
+    //an example , we need to do for all menu items
+    @Then("")
+    public void goto_laptop_notebook() {
         menu.laptop_notebook.clear();
         menu.laptop_notebook.click();
 
