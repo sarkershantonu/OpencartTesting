31f5b1b6b6c42ae162bc59e8a22581de78f60b2b
shantonu sarker <shantonu_oxford@yahoo.com>
2018-05-12 12:43:02 -0400

    changing steps in  cucumber

--- src/test/java/org/automation/CucumberTest.java
+++ src/test/java/org/automation/CucumberTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "html:target/cucumberReport","json:target/cucumber.json" }
-        , features = {"src/test/resources/org/automation/compatibility.feature"}
+        , features = {"src/test/resources/features/compatibility.feature"}
         , glue = {""})
 public class CucumberTest {
 }
