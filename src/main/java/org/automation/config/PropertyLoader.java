309c087d12f6ae4884dc1c4426eface9f3e2da3b
shantonu sarker <shantonu_oxford@yahoo.com>
2018-05-12 16:44:49 -0400

    properties

--- src/main/java/org/automation/config/PropertyLoader.java
+++ src/main/java/org/automation/config/PropertyLoader.java
@@ -12,37 +12,52 @@ public class PropertyLoader {
     public static void loadProperties(String path) throws IOException {
         Properties p = new Properties();
         p.load(new FileInputStream(new File(path)));
-        for(String k:p.stringPropertyNames()){
-            System.setProperty(k,p.getProperty(k));
+        for (String k : p.stringPropertyNames()) {
+            System.setProperty(k, p.getProperty(k));
         }
     }
-    public static void initProperties(){
+
+    public static void initProperties() {
         try {
-            PropertyLoader.loadProperties("./properties/webdriver.properties");
-            PropertyLoader.loadProperties("./properties/cucumber.properties");
-            PropertyLoader.loadProperties("./properties/log4j.properties");
-            PropertyLoader.loadProperties("./properties/test.properties");
-          //  propertiesBasedOnOS();
+            loadProperties("./properties/test.properties");
+            // loadProperties("./properties/webdriver.properties");
+            loadProperties("./properties/cucumber.properties");
+            if ("local".equals(System.getProperty("test.execution"))) {
+                loadProperties("./properties/webdriver.local.properties");
+            } else if ("remote".equals(System.getProperty("test.execution"))) {
+                loadProperties("./properties/webdriver.remote.properties");
+            } else {
+                System.out.println("Execution Property(test.execution) not added @ test.properties = ");
+            }
+             propertiesBasedOnOS();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    private static void propertiesBasedOnOS(){
+
+
+    private static void propertiesBasedOnOS() {
         final String os = JavaProperties.OS_NAME;
-        if (os.contains("win")) {
-            System.setProperty("webdriver.firefox.bin",System.getProperty("firefox.bin.win"));
+        if (os.contains("Windows")) {
+            System.setProperty("webdriver.firefox.bin", System.getProperty("firefox.bin.win"));
             System.setProperty("webdriver.gecko.driver", System.getProperty("firefox.driver.win"));
             System.setProperty("webdriver.chrome.bin", System.getProperty("chrome.bin.win"));
             System.setProperty("webdriver.chrome.driver", System.getProperty("chrome.driver.win"));
+            System.setProperty("webdriver.ie.driver", System.getProperty("ie.driver.win"));
 
-        }else if(os.contains("mac")){
-            // i dont have mac, give me a MAC as gift and I will add here :)
-        }else {
-            System.setProperty("webdriver.firefox.bin",System.getProperty("firefox.bin.linux"));
+        } else if (os.contains("mac")) {
+            System.setProperty("webdriver.firefox.bin", System.getProperty("firefox.bin.mac"));
+            System.setProperty("webdriver.gecko.driver", System.getProperty("firefox.driver.mac"));
+            System.setProperty("webdriver.chrome.bin", System.getProperty("chrome.bin.mac"));
+            System.setProperty("webdriver.chrome.driver", System.getProperty("chrome.driver.mac"));
+        } else if (os.contains("linux")) {
+            System.setProperty("webdriver.firefox.bin", System.getProperty("firefox.bin.linux"));
             System.setProperty("webdriver.gecko.driver", System.getProperty("firefox.driver.linux"));
             System.setProperty("webdriver.chrome.bin", System.getProperty("chrome.bin.linux"));
             System.setProperty("webdriver.chrome.driver", System.getProperty("chrome.driver.linux"));
 
+        } else {
+          throw new RuntimeException("OS is not Listed");
         }
     }
 }
