e78bf6008166fbbbf5508db6333124c82c8e4f68
shantonu sarker <shantonu_oxford@yahoo.com>
2018-05-12 13:31:06 -0400

    property

--- src/test/java/org/automation/core/TestBase.java
+++ src/test/java/org/automation/core/TestBase.java
@@ -1,5 +1,6 @@
 package org.automation.core;
 
+import org.automation.config.PropertyLoader;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
@@ -11,15 +12,17 @@ public abstract class TestBase {
     protected static WebDriver browser = null;
     @BeforeClass
     public static void initBeforeTest(){
-
+        PropertyLoader.initProperties();
         browser = Browser.getInstance();
-
     }
     @AfterClass
     public static void cleanupTest(){
 
     }
 
+    public TestBase(){
+        PropertyLoader.initProperties();
+    }
     private void initProperty(){
 
     }
