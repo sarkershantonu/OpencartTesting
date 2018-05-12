938abe5eda0132fc270ba62504445670547b2a3f
shantonu sarker <shantonu_oxford@yahoo.com>
2018-05-12 12:42:33 -0400

    updated all properties

--- src/main/java/org/automation/config/PropertyLoader.java
+++ src/main/java/org/automation/config/PropertyLoader.java
@@ -16,4 +16,32 @@ public class PropertyLoader {
             System.setProperty(k,p.getProperty(k));
         }
     }
+    public static void initProperties(){
+        try {
+            PropertyLoader.loadProperties("./properties/driver.properties");
+            PropertyLoader.loadProperties("./properties/log4j.properties");
+            PropertyLoader.loadProperties("./properties/test.properties");
+            propertiesBasedOnOS();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void propertiesBasedOnOS(){
+        final String os = JavaProperties.OS_NAME;
+        if (os.contains("win")) {
+            System.setProperty("webdriver.firefox.bin",System.getProperty("firefox.bin.win"));
+            System.setProperty("webdriver.gecko.driver", System.getProperty("firefox.driver.win"));
+            System.setProperty("webdriver.chrome.bin", System.getProperty("chrome.bin.win"));
+            System.setProperty("webdriver.chrome.driver", System.getProperty("chrome.driver.win"));
+
+        }else if(os.contains("mac")){
+            // i dont have mac, give me a MAC as gift and I will add here :)
+        }else {
+            System.setProperty("webdriver.firefox.bin",System.getProperty("firefox.bin.linux"));
+            System.setProperty("webdriver.gecko.driver", System.getProperty("firefox.driver.linux"));
+            System.setProperty("webdriver.chrome.bin", System.getProperty("chrome.bin.linux"));
+            System.setProperty("webdriver.chrome.driver", System.getProperty("chrome.driver.linux"));
+
+        }
+    }
 }
