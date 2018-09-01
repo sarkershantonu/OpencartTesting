2e5ff7a89d29c3cfb891eec6084e6bb95e65aafc
shantonu <shantonu_oxford@yahoo.com>
2018-09-01 07:17:18 -0400

    enum

--- src/main/java/org/automation/core/Browsers.java
+++ src/main/java/org/automation/core/Browsers.java
@@ -1,4 +1,14 @@
 package org.automation.core;
 
 public enum Browsers {
+    FIREFOX("firefox"), CHROME("chrome"), IE("ie");
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    Browsers(String name) {
+        this.name = name;
+    }
 }
