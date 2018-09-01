b21a1c28046d2a7b047f8355a8dac229b1e42f3e
shantonu <shantonu_oxford@yahoo.com>
2018-09-01 05:29:54 -0400

    sample test case

--- src/test/java/org/automation/tests/TestSearch.java
+++ src/test/java/org/automation/tests/TestSearch.java
@@ -1,7 +1,33 @@
 package org.automation.tests;
 
+import org.automation.config.PropertyLoader;
+import org.automation.core.Browser;
+import org.automation.core.TestBase;
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by shantonu on 12/10/16.
  */
-public class TestSearch {
+public class TestSearch  {
+    protected static WebDriver browser = null;
+    @BeforeClass
+    public static void initBeforeTest(){
+        PropertyLoader.initProperties();
+        browser = Browser.getInstance();
+    }
+    @Before
+    public void init(){
+        browser = Browser.getInstance();
+    }
+    @After
+    public void cleanupTest(){
+        browser.quit();
+    }
+
+    @Test
+    public void testChromeExit(){
+        browser.get("https://shantonusarker.blogspot.com");
+        Assert.assertEquals("A Test Developer's blog",browser.getTitle());
+    }
 }
